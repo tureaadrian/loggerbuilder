024b684a1a0b4a410b1e5041704a912dcaeeaae5
Adrian Turea <tureaadrian27@gmail.com>
2018-08-22 13:58:23 +0300

    change github address

--- loggerbuilder/setup.py
+++ loggerbuilder/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='loggerbuilder',
       version='0.1',
       description='Logger Builder ',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/tureaadrian/loggerbuilder',
       author='Adrian Turea',
       author_email='tureaadrtian27@gmail.com',
       license='MIT',
